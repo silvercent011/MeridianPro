32443b04a0b7b7f600273d116e03dedb76e4149b
Sidney Alex <silvercenturion2@gmail.com>
2022-08-23 23:19:55 -0300

    custom daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#005ee6",
+          secondary: "#ffb31a",
+          accent: "#1FB2A6",
+          neutral: "#120b0d",
+          "base-100": "#262626",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
